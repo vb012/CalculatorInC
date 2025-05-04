e2e51373ca3afe058460a272be988f9b1bc9a4b4
vb012 <v-b@outlook.in>
2025-05-04 23:45:44 +0530

    switch case

--- main.c
+++ main.c
@@ -6,15 +6,35 @@ int main()
     int option, a,b,result;
     printf("Enter a input data\n");
     scanf("%d %d",&a,&b);
-    printf("Enter a option to perform a corressponding arithmatic operation \n 1.Addition \n 2.Subraction\n");
+    printf("Enter a option to perform a corressponding arithmatic operation \n1.Addition \n2.Subraction\n3.Division\n4.Multiplication\n");
     scanf("%d",&option);
-    if(option==1)
+    
+    switch (option)
+    {
+    case 1:
     {
         result = a + b;
+        break;
     }
-    else
+    case 2:
     {
         result = a-b;
+        break;
+    }
+    case 3:
+    {
+        result = a/b;
+        break;
+    }
+    case 4:
+    {
+        result = a*b;
+        break;
+    }
+    default:
+    {
+        printf("you selected a wrong variable\n");
+    }
     }
     printf("Result = %d\n\n",result);
     return 0;
