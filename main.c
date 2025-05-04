eb540e06481cd6cbd468027c116c26428a9229ac
vb012 <v-b@outlook.in>
2025-05-04 14:09:15 +0530

    Basic addition and subraction with if condition

--- main.c
+++ main.c
@@ -3,5 +3,19 @@
 int main()
 {
     printf("Calculator program\n");
+    int option, a,b,result;
+    printf("Enter a input data\n");
+    scanf("%d %d",&a,&b);
+    printf("Enter a option to perform a corressponding arithmatic operation \n 1.Addition \n 2.Subraction\n");
+    scanf("%d",&option);
+    if(option==1)
+    {
+        result = a + b;
+    }
+    else
+    {
+        result = a-b;
+    }
+    printf("Result = %d\n\n",result);
     return 0;
 }
