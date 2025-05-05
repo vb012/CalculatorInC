5e1ac2cc40f9774261897500d7e94365213eb76d
vb012 <v-b@outlook.in>
2025-05-05 23:21:40 +0530

    decimal to binary converter added to the code

--- main.c
+++ main.c
@@ -3,39 +3,63 @@
 int main()
 {
     printf("Calculator program\n");
-    int option, a,b,result;
-    printf("Enter a input data\n");
-    scanf("%d %d",&a,&b);
-    printf("Enter a option to perform a corressponding arithmatic operation \n1.Addition \n2.Subraction\n3.Division\n4.Multiplication\n");
-    scanf("%d",&option);
-    
-    switch (option)
+    int option, a,b,result,calctype;
+    int deci,x,i=0;
+    int arr[8]={0,0,0,0,0,0,0,0};
+    printf("Enter the calculator type\n1.Basic Calculator\n2.Programmer\n");
+    scanf("%d",&calctype);
+
+    if(calctype == 1)
     {
-    case 1:
+            printf("Enter a option to perform a corressponding arithmatic operation \n1.Addition \n2.Subraction\n3.Division\n4.Multiplication\n");
+            scanf("%d",&option);
+            printf("Enter a input data\n");
+            scanf("%d %d",&a,&b);
+            switch (option)
+            {
+            case 1:
+            {
+                result = a + b;
+                break;
+            }
+            case 2:
+            {
+                result = a-b;
+                break;
+            }
+            case 3:
+            {
+                result = a/b;
+                break;
+            }
+            case 4:
+            {
+                result = a*b;
+                break;
+            }
+            default:
+            {
+                printf("you selected a wrong variable\n");
+            }
+            }
+            printf("Result = %d\n\n",result);
+    }
+    if(calctype == 2)
     {
-        result = a + b;
-        break;
+        printf("Enter a 8 bit decimal\n");
+        scanf("%d",&deci);
+        while(deci!=0)
+        {
+            arr[i] = deci % 2;
+            deci = deci/2;
+            i++;
+        } 
+        for(i=7;i>=0;i--)
+        {
+        printf("%d",arr[i]);
+        }
+        printf("\n");
     }
-    case 2:
-    {
-        result = a-b;
-        break;
-    }
-    case 3:
-    {
-        result = a/b;
-        break;
-    }
-    case 4:
-    {
-        result = a*b;
-        break;
-    }
-    default:
-    {
-        printf("you selected a wrong variable\n");
-    }
-    }
-    printf("Result = %d\n\n",result);
+
     return 0;
 }
